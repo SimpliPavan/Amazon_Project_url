024343ce8ad53a74e0720823744052f58ecb7353
Pavan <pskulkarni1991@gmail.com>
2022-06-29 21:18:35 +0000

    Mobile Product list

---
+++ Project/src/project_demo/Amazon_Project.java
@@ -0,0 +1,33 @@
+package project_demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Amazon_Project {
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		String dburl = "jdbc:mysql://localhost:3306/Amazon_Products";
+		String username = "root";
+		String password = "root";
+		String query = "select * from Products;";
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection(dburl, username, password);
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery(query);
+		
+		while(rs.next()) {
+			System.out.print("Sl_No: "+rs.getString("Sl_No")+"\t");
+			System.out.print("Brand: "+rs.getString("Brand") +"\t");
+			System.out.print("Model_Name: "+rs.getString("Model_Name") +"\t");
+			System.out.print("         Price: "  + rs.getString("Price") +  " \t");
+			System.out.println();
+		}
+	}
+	
+
+}
